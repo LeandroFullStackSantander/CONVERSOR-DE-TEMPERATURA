6846eea0745718c17825265e266913bb5e7a517d
Leandro Castro <106716899+LeandroFullStackSantander@users.noreply.github.com>
2023-04-20 21:40:02 -0400

    Add files via upload

---
+++ desafio.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//(FORMULA DA CONVERSAO) DICA C = K - 273
+//O VALOR DO PONTO DE EBULIÇÃO DA AGUA EM KELVIN É DE 373°
+const tempEbulicaoK = 373.0
+
+func main() {
+
+	tempK := tempEbulicaoK
+	tempC := (tempK - 273.0)
+
+	fmt.Printf("A temperatura de ebulição em °K = %g (%T) e a temperatura de ebulição em °C é = %g (%T)", tempK, tempK, tempC, tempC)
+
+}
